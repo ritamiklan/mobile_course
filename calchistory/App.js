354b48d243b306711a03973863fe7799a922e445
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-02-05 10:58:31 +0200

    simple calculator with history

---
+++ calchistory/App.js
@@ -0,0 +1,81 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Button, View, TextInput, FlatList } from 'react-native';
+
+export default function App() {
+  const [result, setResult] = useState(0);
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [data, setData] = useState([]);
+
+  const addNumbers = () => {
+    setResult(parseFloat(num1) + parseFloat(num2))
+    let history = `${num1} + ${num2} = ${parseFloat(num1) + parseFloat(num2)}`;
+    setData([history, ...data])
+  }
+
+  const substractNumbers = () => {
+    setResult(parseFloat(num1) - parseFloat(num2))
+    let history2 = `${num1} - ${num2} = ${parseFloat(num1) - parseFloat(num2)}`;
+    setData([history2, ...data])
+  }
+
+  return (
+    <View style = {styles.container}>
+      <View style = {styles.container}>
+      <Text style = {styles.alertText}>Result = {result}</Text>
+        <TextInput 
+          keyboardType = 'numeric'
+          value = {num1}
+          onChangeText = {txt => setNum1(txt)}
+          style={styles.input}
+        />
+        <TextInput 
+          keyboardType = 'numeric'
+          value = {num2}
+          onChangeText = {txt => setNum2(txt)}
+          style={styles.input}
+        />
+      </View>
+      <View style = {styles.buttonContainer}>
+        <Button title = '+' onPress={addNumbers}/>
+        <Button title = '-' onPress={substractNumbers}/>
+      </View>
+      <View style = {styles.container}>
+        <FlatList 
+        data = {data}
+        renderItem = {({item}) => <Text>{item}</Text> }
+        keyExtractor = {(item, index) => index}
+        />
+
+      </View>
+      <StatusBar style='auto'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonContainer: {
+      flex: 1,
+      width: 100,
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+      alignItems: 'flex-start',
+      justifyContent: 'space-around',
+    },
+  alertText: {
+    fontSize: 18
+  },
+  input: {
+    width:200, 
+    borderColor:'gray', 
+    borderWidth:1
+  }
+});
