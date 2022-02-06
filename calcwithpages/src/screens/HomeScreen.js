914ed43ea28ef455b6c6f83362c9812158288411
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-02-06 09:47:27 +0200

    calculator functions implemented

--- calcwithpages/src/screens/HomeScreen.js
+++ calcwithpages/src/screens/HomeScreen.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
 
+    const [result, setResult] = useState(0);
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [data, setData] = useState([]);
+
+    const addNumbers = () => {
+        let res1 = parseFloat(num1) + parseFloat(num2)
+        let history = `${num1} + ${num2} = ${res1}`
+        setResult(res1)
+        setData([history, ...data])
+      }
+    
+      const substractNumbers = () => {
+        let res2 = parseFloat(num1) - parseFloat(num2)
+        let history2 = `${num1} - ${num2} = ${res2}`
+        setResult(res2)
+        setData([history2, ...data])
+      }
+
 return (
     <View style={styles.container}>
         <View style={styles.container}>
-            <Text>text textttt</Text>
-            <TextInput style = {styles.input} />
-            <TextInput style = {styles.input} />
+            <Text>Result: {result}</Text>
+                <TextInput
+                    keyboardType = 'numeric'
+                    value = {num1}
+                    onChangeText = {txt => setNum1(txt)}
+                    style={styles.input}
+                />
+                <TextInput
+                    keyboardType = 'numeric'
+                    value = {num2}
+                    onChangeText = {txt => setNum2(txt)}
+                    style={styles.input}
+                />
         </View>
         <View style = {styles.buttonContainer}>
-            <Button title = '+' />
-            <Button title = '-' />
+            <Button title = '+' onPress={addNumbers}/>
+            <Button title = '-' onPress={substractNumbers}/>
             <Button title = 'History' onPress={() => navigation.navigate('History')} />
         </View>
         <StatusBar style="auto" />
