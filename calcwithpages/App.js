950dc7270ab62a72a4d62cf585175dfa3a9b70ea
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-02-06 08:42:40 +0200

    added screens and navigation

--- calcwithpages/App.js
+++ calcwithpages/App.js
@@ -1,42 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { NavigationContainer } from'@react-navigation/native';
+import { createNativeStackNavigator } from'@react-navigation/native-stack';
+import HistoryScreen from './src/screens/HistoryScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-      <Text>text textttt</Text>
-      <TextInput style = {styles.input} />
-      <TextInput style = {styles.input} />
-      </View>
-      <View style = {styles.buttonContainer}>
-        <Button title = '+' />
-        <Button title = '-' />
-        <Button title = 'History' />
-      </View>
 
-      <StatusBar style="auto" />
-    </View>
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component = {HomeScreen} />
+        <Stack.Screen name='History' component = {HistoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    width: 200,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-  }
-});
---
+++ calcwithpages/src/screens/HistoryScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function HistoryScreen() {
+
+    return(
+        <View>
+            <Text>History screen</Text>
+        </View>
+    );
+}
