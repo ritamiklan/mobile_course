986fe0ede99d0ab0fedbccc766cd583fef3de552
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-04-03 21:50:41 +0300

    location permission now works

--- mappractice/App.js
+++ mappractice/App.js
@@ -6,32 +6,48 @@ import React, { useState, useEffect } from "react";
 import { Alert } from "react-native-web";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import config from "./config";
+import Constants from "expo-constants";
+
+// region state, region prop
+// text input
+// press button: find coordinates, update reagon state
+//cconst [location, setLocation] = useState(null);
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-
   const [region, setRegion] = useState({
     latitude: 60.200692,
     longitude: 24.934302,
     latitudeDelta: 0.0322,
     longitudeDelta: 0.0221,
   });
-  // region state, region prop
-  // text input
-  // press button: find coordinates, update reagon state
-  //coordinates: api, mapquest
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestBackgroundPermissionsAsync();
-      if (status !== "granted") {
-        Alert.alert("No permission to get location");
+      if (Platform.OS === "android" && !Constants.isDevice) {
+        setErrorMsg(
+          "Oops, this will not work on Snack in an Android emulator. Try it on your device!"
+        );
         return;
       }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
     })();
   }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={{ marginTop: 20, flex: 1 }}>
